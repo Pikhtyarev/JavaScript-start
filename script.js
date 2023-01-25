f5c38d0c86fcb09d6ef740b0e5fcb4111e8d0696
Egor <pihtyarevv@mail.ru>
2023-01-25 19:50:28 +0300

    Размещение депозита

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("Hello")
+const startMoney = 12000;
+const percent = 0.07;
+const finishMoney = (startMoney * (1 + percent) ** 24);
+const housePrice = 13500;
+
+if (finishMoney > housePrice) {
+    console.log('Василий сможет купить дом за $13500');
+} else {
+    console.log('Ваcилий не сможет купить дом за $13500');
+}
+
+console.log(finishMoney);
+console.log(`Остаток после покупки $${finishMoney - housePrice}`);
