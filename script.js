444ee2e984e84d0732521e8b93a3a1815bd81e42
Egor <pihtyarevv@mail.ru>
2023-01-26 10:54:07 +0300

    Проверка на робота

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const startMoney = 12000;
-const percent = 0.07;
-const finishMoney = (startMoney * (1 + percent / 12) ** 24);
-const housePrice = 13500;
+let answer = prompt('Сколько будет 7+15 или 7-15?');
 
-if (finishMoney > housePrice) {
-    console.log('Василий сможет купить дом за $13500');
-} else {
-    console.log('Ваcилий не сможет купить дом за $13500');
+switch (true) {
+    case Number(answer) === 22:
+    case Number(answer) === -8:
+    case answer === 'Я не робот':
+        alert('Успех');
+    break;
+    default : alert('Вы робот');
 }
-
-console.log(`Остаток после покупки $${finishMoney - housePrice}`);
